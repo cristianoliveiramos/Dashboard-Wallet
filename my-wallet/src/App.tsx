dd850143dacbbb3c0104b5ec3cd4b5569f063bb0
cristianoliveiramos <cristianoliveiramos@gmail.com>
2025-11-21 20:04:37 -0300

    Implemented grid layout

---
+++ my-wallet/src/App.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./components/Aside/styles"
+import Layout from "./components/Layout"
+import Dashboard from "./pages/Dashboard"
+
+export const App = () => {
+    return (
+        <>
+            <Layout/>         
+        </>
+    )
+}
--- my-wallet/src/index.tsx
+++ my-wallet/src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Dashboard from './pages/Dashboard';
+import { App } from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Dashboard/>
+    <App />
   </React.StrictMode>
 );
 
