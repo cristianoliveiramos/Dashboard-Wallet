aab070a16b779cee316dbbb1cc0e9d69fa988f19
cristianoliveiramos <cristianoliveiramos@gmail.com>
2025-11-23 20:27:15 -0300

    Changed App organization. Initiated theme construction

---
+++ my-wallet/src/styles/styled.ts
@@ -0,0 +1,19 @@
+import 'styled-components'
+
+export interface DefaultTheme {
+    title: string;
+
+    colors: {
+        primary: string,
+        secondary: string,
+        tertiary: string,
+
+        white: string,
+        black: string,
+        grey: string,
+
+        success: string,
+        info: string,
+        warning: string
+    }
+}
