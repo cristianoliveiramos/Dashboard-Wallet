7ae96852833f4aa75db8cfdf4f6a431764415022
cristianoliveiramos <cristianoliveiramos@gmail.com>
2025-12-08 20:14:31 -0300

    Implemented Aside Menu Component and Syled Component.

--- my-wallet/src/components/Aside/Aside.tsx
+++ my-wallet/src/components/Aside/Aside.tsx
@@ -1,10 +1,51 @@
-import "./styles"
-import { Container } from "./styles"
+import "./styles";
+import {  
+  MdArrowUpward,
+  MdDashboard,
+  MdExitToApp,
+  MdArrowDownward
+} from "react-icons/md";
+
+import {
+  AsideWrapper,
+  Header,
+  LogImg,
+  MenuAside,
+  MenuItemLink,
+  Title,
+} from "./styles";
+
+import logImg from "../../assets/logo.svg";
 
 export const Aside = () => {
-    return (
-        <Container>
-            <h1>ASIDE</h1>
-        </Container>
-    )
-}
+  return (
+    <AsideWrapper>
+      <Header>
+        <LogImg src={logImg} alt="Logo Minha Carteira"></LogImg>
+        <Title>Minha Carteira</Title>
+      </Header>
+        
+      <MenuAside>
+				<MenuItemLink href="#">  
+					<MdDashboard />        
+					Dashboard            
+				</MenuItemLink>        
+
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+          Saídas
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuAside>      
+    </AsideWrapper>
+  );
+};
